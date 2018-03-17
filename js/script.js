8e3a311bae83d2858e59c7ca3b24184c49507a9e
Marc-Hu <marc.hu@outlook.fr>
2018-03-17 17:27:11 +0100

    add Nadir part

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ $(document).ready(function() {
         console.log("Appuie sur le bouton commentaire");
         if($(this).parent().find('.commentaire-item').first().is(":visible")){
             $(this).parent().find('.commentaire-item').first().hide();
+            $(this).text('+ Commentaires');
         } else {
             $(this).parent().find('.commentaire-item').first().show();
+            $(this).text('- Commentaires');
         }
     });
 })
